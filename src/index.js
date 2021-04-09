6924761dcd9ec0fd5421c6036dab2c2446402f45
Mendel <mendeljacks@gmail.com>
2021-04-08 22:35:47 -0400

    game complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
+import {createMuiTheme, ThemeProvider} from '@material-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
+import {Main} from './main.jsx';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#927ba9',
+    }
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+    <Main />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
